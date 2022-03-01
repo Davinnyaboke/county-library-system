b645a6679d396090c5018a182154aa1bb5c62bde
Davinnyaboke <98393716+Davinnyaboke@users.noreply.github.com>
2022-03-01 17:30:25 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,84 +1,44 @@
-
 /*
-    Project: County Library management system.
-    Author:  Sherryl Arshlyne
-    Date:    Oct,2021
-    Compiler: GNU GCC
-    Language: C99
-    License: MIT
-    */
+  county library systems
+  marshmellow
+  Jan,2021
+  Compiler: GNU GNC
+  Language C89
+  License: MIT
 
+*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 
-int menu();
-void execute_action(int action);
-int main()
-{
+int main() {
     int action;
-    action = menu();
-    execute_action(action);
-    return 0;
-}
+    printf("select an action below\n");
+    printf("1. Add new patron\n");
+    printf("2. view all patrons\n");
+    printf("3. View all books\n");
+    printf("4. Add new Book\n");
+    printf("Your action: \n");
+    if(action< 1|| action>4)
+      printf("invalid action. Try again\n");
 
-void execute_action (int action) {
-switch (action){
+      return 0;
+ void execute_action(int action){
+    switch(action){
     case 1:
-        printf("List of users\n");
+        printf("adding a new patron\n");
         break;
     case 2:
-        printf("Add new user\n");
+        printf("list of all patrons\n");
         break;
     case 3:
-        printf("Editing user\n");
+        printf("list os all books\n");
         break;
     case 4:
-        printf("Deleting User\n");
+        printf("adding a new book\n");
         break;
-    case 5:
-        printf("Changing password\n");
-        break;
-    case 6:
-        printf("You have been logged out\n");
-        break;
-    case 7:
-        close();
-        break;
-    default:
-        printf("Invalid action\n");
+    default: printf(" invalid action.\n");
     }
+ }
 
-
-   void close() {
-    printf("Thanks for using the system\n");
-    printf("Created by Titus Njiru\n");
-    printf("Bye\n");
-    Sleep(2000);
-    exit(0);
+    return 0;
 }
-
-
-int menu() //header/prototype
-{
-            int action;
-
-        do {
-        printf("\tCounty Library\n");
-        printf("Welcome Mr. Titus. \n");
-        printf("What would you like to do?\n");
-        printf("1. View Users.\n");
-        printf("2. Add new User\n");
-        printf("3. Edit User.\n");
-        printf("4. Delete User.\n");
-        printf("5. Change Password.");
-        printf("6. Logout.\n");
-        printf("7. Exit.\n");
-        printf("Selected action(1-7): ");
-        scanf("%d",&action);
-        if(action < 1 || action > 7) {
-            printf("Invalid Action. Try again.\n");
-        }
-}while (action < 1 || action > 7);
-}
-
